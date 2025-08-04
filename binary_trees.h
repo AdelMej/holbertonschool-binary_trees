3cd3d845cdb13a19faec5bf63ccf02222d23c74d
AdelMej <adelmejrissi@gmail.com>
2025-08-04 15:16:06 +0200

    feat: added a new function that deletes a tree and it's children

--- binary_trees.h
+++ binary_trees.h
@@ -28,8 +28,9 @@ typedef struct binary_tree_s heap_t;
 /* --- constructor --- */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
-/* --- binary tree utilities --- */
+/* --- binary trees utilities --- */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif /* MY_BINARY_TREES_H */
