49b1993a2ae2331a3a15ee93152917450f93e6df
AdelMej <adelmejrissi@gmail.com>
2025-08-04 17:06:39 +0200

    feat: added prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -34,4 +34,12 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 
+/* --- traversal functions --- */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* --- size, heigth, depth --- */
+size_t binary_tree_height(const binary_tree_t *tree);
+
 #endif /* MY_BINARY_TREES_H */
