fb3619ae47146f18abef47c317a9fd4f3892fec7
AdelMej <adelmejrissi@gmail.com>
2025-08-04 13:43:31 +0200

    feat added doc for betty standard

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,14 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 
+/**
+* binary_tree_node - a function that create a new node
+* @parent: the parent of the new node
+* @value: the value of the new node
+*
+* Return: the new node
+* NULL if it fails
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
