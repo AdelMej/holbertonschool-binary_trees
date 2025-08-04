6e4395e66bbb0b233af987f784d372590e106034
AdelMej <adelmejrissi@gmail.com>
2025-08-04 16:36:25 +0200

    fix: removes useless test

--- tests/test_binary_tree_delete.c
+++ tests/test_binary_tree_delete.c
@@ -9,17 +9,6 @@ void tearDown(void)
 {
 }
 
-void test_deleting_big_tree (void)
-{
-	binary_tree_t *toTest = NULL;
-
-	toTest = binary_tree_node(toTest, 45);
-	toTest->left = binary_tree_node(toTest->left, 45);
-	toTest->right = binary_tree_node(toTest->right, 45);
-	binary_tree_delete(toTest);
-
-	TEST_ASSERT_NULL(toTest);
-}
 
 void test_give_null(void)
 {
@@ -32,7 +21,6 @@ void test_give_null(void)
 int main(void)
 {
     UNITY_BEGIN();
-	RUN_TEST(test_deleting_big_tree);
 	RUN_TEST(test_give_null);
     return UNITY_END();
 }
