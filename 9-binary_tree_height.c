9b5a9fb42ab068cbb99b46d4773851061dbd2d36
AdelMej <adelmejrissi@gmail.com>
2025-08-05 15:33:59 +0200

    feat: added betty style documentation

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_height - a function that returns the heigh of the binary tree
+* @tree: the root of the binary tree
+*
+* Return: 0 if NULL or a leaf
+* maxHeight + 1 recursively
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left = 0, right = 0, maxHeight = 0;
@@ -7,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if(binary_tree_is_leaf(tree))
+	if (binary_tree_is_leaf(tree))
 		return (0);
 
 	left = binary_tree_height(tree->left);
