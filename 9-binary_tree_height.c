7f3c57272946904badd8fa8d784bd85327749680
AdelMej <adelmejrissi@gmail.com>
2025-08-06 11:34:36 +0200

    feat: return to old version because it should be correct

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,20 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left = 0, right = 0;
+	size_t left = 0, right = 0, maxHeight = 0;
 
 	if (tree == NULL)
 		return (0);
 
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
-	return ((left >= right ? left : right) + ((tree->left || tree->right) ? 1 : 0));
+	maxHeight = left;
+	if (left < right)
+		maxHeight = right;
+
+	return (maxHeight + 1);
 }
