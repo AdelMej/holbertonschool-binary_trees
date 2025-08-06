d892229bc82bfab6d1e59e79abe1bfa78260b007
AdelMej <adelmejrissi@gmail.com>
2025-08-06 10:43:30 +0200

    fix: feat issues with is leaf

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,7 +11,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t node_numbers = 0;
 
-	if (tree == NULL || binary_tree_is_leaf(tree))
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
 	node_numbers += binary_tree_nodes(tree->left);
